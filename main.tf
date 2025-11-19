3af7ea06e07ab48df045b59bd6634237b99dfa1b
wellsgz <wellsgz@hotmail.com>
2025-11-19 18:07:49 +0800

    add terraform cloud for tfstate

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  cloud {
+    organization = "network-as-code"
+
+    workspaces {
+      name = "nac-meraki-terraform"
+    }
+  }
+  required_providers {
+    meraki = {
+      source  = "cisco-open/meraki"
+      version = ">= 1.0.0"
+    }
+  }
+}
+
 module "meraki" {
   source  = "netascode/nac-meraki/meraki"
   version = "0.1.0"
